b49c63029fb2a0457469d0c1e8fe385fd3eca927
admin <admin@tak.local>
2018-07-08 16:44:01 +0800

    botsss

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,14 @@ export const app: express.Express = express();
 // app.use(express.static(DIST_DIR));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
-// var allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// }
-// app.use(allowCrossDomain);
+const allowCrossDomain = function(req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
+
 setUpBotWebHooks();
 app.get("/", function(req: any, res: any) {
     res.send("MiniSquadron Bot Server");
